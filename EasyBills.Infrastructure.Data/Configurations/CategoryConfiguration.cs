55ead39c3aa75bf78b19ce4f049c605e0c4e740f
moises-herrera <moisesherrera037@gmail.com>
2023-04-11 10:26:48 -0500

    feat: add category config

---
+++ EasyBills.Infrastructure.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EasyBills.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyBills.Infrastructure.Data.Configurations;
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(c => c.Name)
+            .HasMaxLength(80)
+            .IsRequired();
+
+        builder.Property(c => c.Description)
+            .HasMaxLength(150);
+    }
+}
