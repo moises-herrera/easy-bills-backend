1fe589cfa10f893851e4cb7f27421a16b9e50063
moises-herrera <moisesherrera037@gmail.com>
2023-04-27 17:32:19 -0500

    refactor: account DTO

--- EasyBills.Application/Accounts/AccountDTO.cs
+++ EasyBills.Application/Accounts/AccountDTO.cs
@@ -7,12 +7,14 @@ namespace EasyBills.Application.Accounts;
 ///  Account DTO to render data.
 /// </summary>
 /// <param name="Id">Id.</param>
+/// <param name="Name">Account name.</param>
 /// <param name="TypeAccount">Type of account.</param>
 /// <param name="Balance">Balance.</param>
 /// <param name="UserId">User id.</param>
 /// <param name="Transactions">Transactions.</param>
 public record AccountDTO(
-    Guid Id, 
+    Guid Id,
+    string Name,
     FinanceAccountType TypeAccount,
     decimal Balance,
     Guid UserId,
