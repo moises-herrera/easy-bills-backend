8f2b3130f82b7bd578975dca97710fc6c76e35e4
moises-herrera <moisesherrera037@gmail.com>
2023-04-28 18:19:06 -0500

    refactor: update transaction DTO

--- EasyBills.Application/Transactions/TransactionDTO.cs
+++ EasyBills.Application/Transactions/TransactionDTO.cs
@@ -3,12 +3,14 @@
 /// <summary>
 /// Trasaction DTO to render data.
 /// </summary>
+/// <param name="Id">Transaction id.</param>
 /// <param name="Amount">Amount.</param>
 /// <param name="Description">Description.</param>
 /// <param name="AccountId">Account id.</param>
 /// <param name="CategoryId">Category id.</param>
 /// <param name="IsIncome">If is an income.</param>
 public record TransactionDTO(
+    Guid Id,
     decimal Amount,
     string Description,
     Guid AccountId,
